3c25226324df8067a93c5aa43f8193f158973f95
Matheus <mths.ss@msn.com>
2014-10-17 00:16:57 -0300

    auths.py finalizado
    A não ser para implementar novos recursos, não há mais necessidade de
    modificar esse arquivo.

--- ufuinfo/auths.py
+++ ufuinfo/auths.py
@@ -20,13 +20,10 @@
 #  along with UFUInfo.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-"""Módulo de autenticações
+u"""Módulo de autenticações.
 
-Métodos de log-in e log-out para os sistemas da instituição que necessitem
-de autenticação.
-
-Depende da biblioteca Beautiful Soup 4 e Python 2.7
-
+Este módulo contém métodos de log-in e log-out para os sistemas da instituição
+que necessitem de autenticação para serem utilizados.
 """
 
 import urllib
@@ -34,38 +31,58 @@ import urllib2
 import cookielib
 from bs4 import BeautifulSoup
 
-class AuthBiblioteca:
 
-    """Métodos de log-in e log-out para o SISBI, Sistema de Bibliotecas da UFU.
+class UsuarioSenhaIncorretos(Exception):
 
-    O Sistema de Bibliotecas incrivelmente NÃO suporta HTTPS. As requisições tem
-    de serem feitas utilizando HTTP, que expõe a senha a ataques man-in-the-middle.
+    u"""Exceção para quando usuário e/ou senha informados estão incorretos."""
 
-    Por favor, alertar possíveis usuários sobre isso.
+    def __str__(self):
+        return repr('Usuario e/ou senha estao incorretos, tente novamente.')
+
+
+class AuthBiblioteca(object):
+
+    u"""Métodos de log-in e log-out para o SISBI, o Sistema de Bibliotecas.
+
+    Note:
+
+        Note que o SISBI **NÃO** suporta HTTPS. As requisições são feitas
+        utilizando HTTP, o que expõe a senha a ataques *man-in-the-middle*.
+
+        Por favor, alertar possíveis usuários sobre isso.
     """
 
     def __init__(self):
-
-        """Inicializa o objeto com a URL do form, uma cookie jar e um opener"""
-
-        self.url = 'http://babao.dr.ufu.br:8080/auth/login?wicket:bookmarkablePage'+\
-        '=:com.vtls.chamo.webapp.component.opac.OpacLoginPage&wicket:interface=:0:'+\
-        'loginPanel:loginForm::IFormSubmitListener::'
+        u"""Inicializa o objeto com a URL, uma *cookie jar* e um *opener*."""
+        self.url = ('http://babao.dr.ufu.br:8080/auth/login?wicket:'
+                    'bookmarkablePage=:com.vtls.chamo.webapp.component.opac.'
+                    'OpacLoginPage&wicket:interface=:0:loginPanel:loginForm::'
+                    'IFormSubmitListener::')  # HTTP, really?
         self.cookies = cookielib.CookieJar()
-        self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cookies))
-
+        self.cookie_processor = urllib2.HTTPCookieProcessor(self.cookies)
+        self.opener = urllib2.build_opener(self.cookie_processor)
 
     def log_in(self, usuario, senha):
+        u"""Método de log-in ao sistema.
 
-        """Método de login
+        Arguments:
 
-        Argumentos:
+            usuario (str): Usuário cadastrado no SISBI, não há padronização.
+            senha (str): Senha do usuário. D'oh!
 
-        usuario -- Usuário do SISBI
-        senha -- Senha do SISBI
+        Raises:
 
+            urllib2.URLError: Caso o adaptador de rede não estiver conectado.
+            UsuarioSenhaIncorretos: Quando usuário e/ou senha estão incorretos.
+
+        Returns:
+
+            Uma instância do "opener" capaz de abrir novas URL com os cookies
+            salvos, na sessão aberta.
+
+                .open(): Abre uma nova URL.
+                .open().read(): Retorna o código dessa página.
         """
-
         dados = urllib.urlencode({'username': usuario, 'password': senha})
         pag = self.opener.open(self.url, dados).read()
         soup = BeautifulSoup(pag)
@@ -75,13 +92,15 @@ class AuthBiblioteca:
 
         return self.opener
 
-
     def log_out(self):
-        """Método de log-out"""
-        self.opener.open('http://babao.dr.ufu.br:8080/auth/logout?theme=system')
+        u"""Método de log-out.
 
-
-class UsuarioSenhaIncorretos(Exception):
-    """Exceção lançada quando usuário e/ou senha informados estão incorretos"""
-    def __str__(self):
-        return repr('Usuário e/ou senha estão incorretos, tente novamente.')
+        Este método existe apenas por formalidades. Não retorna nada, nem há a
+        necessidade de checar se foi exitoso.
+        """
+        try:
+            self.opener.open('http://babao.dr.ufu.br:8080/auth/logout')
+        except urllib2.HTTPError:
+            pass
+        except urllib2.URLError:
+            pass
