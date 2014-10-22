c8b5a167c7151829ecd7ea579f77199ad09703b9
Matheus <mths.ss@msn.com>
2014-10-21 22:03:25 -0200

    __init__.py

---
+++ ufuinfo/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#
+#                              Módulo de __init__
+#
+#  This file is part of UFUInfo
+#
+#  Copyright (C) 2014  Matheus Silva Santos
+#
+#  UFUInfo is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  UFUInfo is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with UFUInfo.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+u"""UFUInfo.
+
+UFUInfo é uma biblioteca Python (2.7) capaz de recuperar informações disponí-
+veis nos sites da UFU, a Universidade Federal de Uberlândia. Seus parsers
+retornam dictionaries devidamente aninhados, seguindo os padrões definidos pela
+documentação.
+"""
